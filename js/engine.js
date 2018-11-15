473df25e0fda072ae35fc0baef4d63adb018f37d
Cristian Saguil <cristian.saguil@gmail.com>
2018-11-14 22:28:07 -0500

    made startButton global:

--- js/engine.js
+++ js/engine.js
@@ -6,11 +6,11 @@
 //globals
 const Direction = {"up": 1, "down": 2, "right": 3, "left": 4}; //Direction enum
 var dir; //current direction snake is travelling
+var startButton = document.getElementById("start-button");
 
 (function() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  var startButton = document.getElementById("start-button");
 
   startButton.addEventListener("click", function() {
     var service = SnakeGameService(ctx, 500, 500, 10, 25);
